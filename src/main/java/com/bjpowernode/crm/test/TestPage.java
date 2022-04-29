c7669787db93517dcd8481a2487d3b600ed86cda
Shea <Shea@bjpowernode.com>
2022-04-29 16:40:29 +0800

    test third commit

--- src/main/java/com/bjpowernode/crm/test/TestPage.java
+++ src/main/java/com/bjpowernode/crm/test/TestPage.java
@@ -3,5 +3,8 @@ package com.bjpowernode.crm.test;
 public class TestPage {
     public static void main(String[] args) {
         System.out.println("Git......01");
+
+
+        System.out.println("Git......03");
     }
 }
